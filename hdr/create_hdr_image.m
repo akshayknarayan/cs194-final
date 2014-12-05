1267b92202df004e233454211f5cbfb3c1faff8f
Japheth Wong <japhethjwong@gmail.com>
2014-12-04 21:11:45 -0800

    Adding calls to gsolve

--- hdr/create_hdr_image.m
+++ hdr/create_hdr_image.m
@@ -2,7 +2,7 @@
 % Name:  Japheth Wong and Akshay Narayan
 % Login: cs194-fb and cs194-ka
 
-function result = create_hdr_image(directory, extension)
+function result = create_hdr_image(directory, extension, lambda)
     % Read in images and exposure times from directory.  Take the log of exposure time.
     [images, exposure_times] = read_images(directory, extension);
     ln_dt = log(exposure_times);
@@ -13,9 +13,10 @@ function result = create_hdr_image(directory, extension)
     % Compute the weighting function needed.
     weights = compute_weights();
 
-
     % Solve for the camera response for each color channel.
-
+    [g_red, le_red] = gsolve(z_red, ln_dt, lambda, weights);
+    [g_green, le_green] = gsolve(z_green, ln_dt, lambda, weights);
+    [g_blue, le_blue] = gsolve(z_blue, ln_dt, lambda, weights);
 
     % Compute the HDR radiance map.
 
