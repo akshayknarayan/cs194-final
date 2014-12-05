5dd018cacca3596cbe359c2ec213b31975f5166d
Japheth Wong <japhethjwong@gmail.com>
2014-12-04 20:53:20 -0800

    Added sampling call to the create HDR image function

---
+++ hdr/create_hdr_image.m
@@ -0,0 +1,23 @@
+% CS194-26 Final Project: High Dynamic Range
+% Name:  Japheth Wong and Akshay Narayan
+% Login: cs194-fb and cs194-ka
+
+function result = create_hdr_image(directory, extension)
+    % Read in images and exposure times from directory.  Take the log of exposure time.
+    [images, exposure_times] = read_images(directory, extension);
+    ln_dt = log(exposure_times);
+
+    % Sample the images appropriately, per color channel.
+    [z_red, z_green, z_blue] = sample_rgb_images(images);
+
+    % Compute the weighting function needed.
+    
+
+    % Solve for the camera response for each color channel.
+
+
+    % Compute the HDR radiance map.
+
+
+
+end
