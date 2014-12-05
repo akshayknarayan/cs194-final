048b4a75d1612636c48422fb5c542a1fdf754fa5
Japheth Wong <japhethjwong@gmail.com>
2014-12-04 20:52:54 -0800

    Corrected what seems to be an error in the provided gsolve function

--- hdr/gsolve.m
+++ hdr/gsolve.m
@@ -42,19 +42,22 @@ function [g,lE] = gsolve(Z,B,l,w)
             wij = w(Z(i,j)+1);
             A(k,Z(i,j)+1) = wij; 
             A(k,n+i) = −wij; 
-            b(k,1) = wij * B(i,j);
-            k=k+1;
+            % b(k,1) = wij * B(i,j);
+            b(k,1) = wij * B(j);
+            k = k+1;
         end
     end
 
     %% Fix the curve by setting its middle value to 0
     A(k,129) = 1;
-    k=k+1;
+    k = k+1;
 
     %% Include the smoothness equations
-    for i=1:n−2
-        A(k,i)=l*w(i+1); A(k,i+1)=−2*l*w(i+1); A(k,i+2)=l*w(i+1);
-        k=k+1;
+    for i = 1:n−2
+        A(k,i) = l * w(i+1); 
+        A(k,i+1) = −2 * l * w(i+1); 
+        A(k,i+2) = l * w(i+1);
+        k = k+1;
     end
 
     %% Solve the system using SVD
